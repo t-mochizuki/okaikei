0bd48f41afc6457c7353bef16e181a655b7d452a
t-mochizuki <119Mochizuki@gmail.com>
2015-02-19 22:11:06 +0900

    Add tests for deleting a record

--- test/models/credit_test.rb
+++ test/models/credit_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CreditTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A record in the database is deleted" do
+    entry = Entry.create(date: Date.new(2015, 2, 19), comment: "hoge")
+    entry_id = entry.id
+    credit = Credit.create(name: "cname", money: 100, entry_id: entry_id)
+    Debit.create(name: "dname", money: 200, entry_id: entry_id)
+
+    credit.destroy
+
+    assert Entry.find_by(id: entry_id)
+    assert Credit.find_by(entry_id: entry_id).nil?
+    assert Debit.find_by(entry_id: entry_id)
+  end
 end
--- test/models/debit_test.rb
+++ test/models/debit_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class DebitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A record in the database is deleted" do
+    entry = Entry.create(date: Date.new(2015, 2, 19), comment: "hoge")
+    entry_id = entry.id
+    Credit.create(name: "cname", money: 100, entry_id: entry_id)
+    debit = Debit.create(name: "dname", money: 200, entry_id: entry_id)
+
+    debit.destroy
+
+    assert Entry.find_by(id: entry_id)
+    assert Credit.find_by(entry_id: entry_id)
+    assert Debit.find_by(entry_id: entry_id).nil?
+  end
 end
