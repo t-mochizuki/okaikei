3530f9d4b092372e7ee04211c5d3e8e912949908
t-mochizuki <119Mochizuki@gmail.com>
2015-02-19 20:05:48 +0900

    Remove the save method
    the create method call will create and save a new record into the
    database

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -2,25 +2,13 @@ require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
   test "Records in the database is deleted" do
-    entry = Entry.create
+    entry = Entry.create(date: Date.new(2015, 2, 19), comment: "hoge")
     entry_id = entry.id
-    entry.date = Date.new(2015, 2, 19)
-    entry.comment = "hoge"
-    entry.save
-
-    credit = Credit.create
-    credit.name = "cname"
-    credit.money = 100
-    credit.entry_id = entry_id
-    credit.save
-
-    debit = Debit.create
-    debit.name = "dname"
-    debit.money = 200
-    debit.entry_id = entry_id
-    debit.save
+    Credit.create(name: "cname", money: 100, entry_id: entry_id)
+    Debit.create(name: "dname", money: 200, entry_id: entry_id)
 
     entry.destroy
+
     assert Entry.find_by(id: entry_id).nil?
     assert Credit.find_by(entry_id: entry_id).nil?
     assert Debit.find_by(entry_id: entry_id).nil?
