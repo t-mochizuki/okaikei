eb4fe025da999d5c3989f383d9cbea61894c7257
t-mochizuki <119Mochizuki@gmail.com>
2015-02-19 19:53:47 +0900

    Add a test for deleting recodes

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Records in the database is deleted" do
+    entry = Entry.create
+    entry_id = entry.id
+    entry.date = Date.new(2015, 2, 19)
+    entry.comment = "hoge"
+    entry.save
+
+    credit = Credit.create
+    credit.name = "cname"
+    credit.money = 100
+    credit.entry_id = entry_id
+    credit.save
+
+    debit = Debit.create
+    debit.name = "dname"
+    debit.money = 200
+    debit.entry_id = entry_id
+    debit.save
+
+    entry.destroy
+    assert Entry.find_by(id: entry_id).nil?
+    assert Credit.find_by(entry_id: entry_id).nil?
+    assert Debit.find_by(entry_id: entry_id).nil?
+  end
 end
