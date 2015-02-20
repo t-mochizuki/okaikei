027aeef1a81dceb0782057169c490eb7532dee5e
t-mochizuki <119Mochizuki@gmail.com>
2015-02-20 21:44:27 +0900

    Add a show page of a entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,4 +2,8 @@ class EntriesController < ApplicationController
   def index
     @entries = Entry.all
   end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'entries/index'
 
+  get 'entries/show/:id', to: 'entries#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
